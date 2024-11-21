3b87da7b3ca1219b6467105f8b803a11b97707e2
Sukhleen <sukhleenk1113@gmail.com>
2024-11-21 13:17:19 -0600

    add all memorization strengths  values to array

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import statistics
 with open('example.txt', 'r') as f:
 
     FID = []
+    mem_strength = []
 
     for line in f:
         if 'FID' in line:
@@ -14,3 +15,15 @@ with open('example.txt', 'r') as f:
                     if number:
                         FID.append(float(number))
                         break
+
+        if 'memorization_strength' in line:
+            number1 = ""
+            for char in line:
+                for char in line:
+                if char.isdigit() or char == '.':
+                    number += char
+                else:
+                    if number:
+                        mem_strength.append(float(number))
+                        break
+                
